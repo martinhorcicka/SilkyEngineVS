cbcf6811ad1d50f7c5a849123f6cf8566a378164
martinhorcicka <horcicka.mar@email.cz>
2020-04-28 11:54:03 +0200

    Přidejte soubory projektu.

---
+++ SilkyEngineVS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SilkyEngineVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
